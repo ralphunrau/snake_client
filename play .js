234d9da75e1ff851e8e31d39e50386abeb2f7912
Ralph Unrau <xralphunrau@gmail.com>
2022-03-16 19:54:42 +0000

    creating connection with snake server

--- play .js
+++ play .js
@@ -1,19 +1,6 @@
 const net = require('net');
-
-const connect = () => {
-const conn =  net.createConnection({
-  host: '165.227.47.243',
-  port: '50541'
-});
-
-conn.on("data", (data) => {
-  console.log(data)
-});
-
-conn.setEncoding('utf8');
-
-return conn;
-};
+const conn = require('./client')
+connect = conn.connect
 
 console.log('Connecting ...');
 connect();
